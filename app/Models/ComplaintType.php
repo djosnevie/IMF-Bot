22c588977a66bb32afb7c1152a7bbb0d27246e37
djosnevie <djodjokabongo309@gmail.com>
2026-08-03 19:22:32 +0100

    fix: correction du namespace LogsActivity

--- app/Models/ComplaintType.php
+++ app/Models/ComplaintType.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class ComplaintType extends Model
 {
